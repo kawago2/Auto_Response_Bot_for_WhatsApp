ba863a6036c10b4ff6c33b70b3b24af331d8e22d
kawago2 <itkawago@gmail.com>
2022-08-27 22:38:48 +0700

    add bar top and reply message

--- wa.py
+++ wa.py
@@ -9,18 +9,20 @@ position1 = pt.locateOnScreen("smiley.png", confidence=.6)
 x = position1[0]
 y = position1[1]
 
-#gets message
+# gets message
+
+
 def get_message():
-    global x,y
+    global x, y
 
     postion = pt.locateOnScreen("smiley.png", confidence=.6)
     x = position1[0]
     y = position1[1]
-    pt.moveTo(x,y, duration=.05)
-    pt.moveTo(x + 70, y - 40, duration = .5)
+    pt.moveTo(x, y, duration=.05)
+    pt.moveTo(x + 70, y - 40, duration=.5)
     pt.tripleClick()
     pt.rightClick()
-    pt.moveRel(12,15)
+    pt.moveRel(12, 15)
     pt.click()
     whatsapp_message = pyperclip.paste()
     pt.click()
@@ -28,7 +30,9 @@ def get_message():
 
     return whatsapp_message
 
-#posts
+# posts
+
+
 def post_response(message):
     global x, y
 
@@ -41,53 +45,65 @@ def post_response(message):
 
     pt.typewrite("\n", interval=.01)
 
-#process response
+# after send message
+
+
+def after_end_message():
+    global x, y
+
+    position = pt.locateOnScreen("bar_top.png", confidence=.6)
+    x = position[0]
+    y = position[1]
+    pt.moveTo(x, y+80, duration=.5)
+    pt.click()
+
+# process response
+
+
 def process_response(message):
     random_no = random.randrange(3)
 
-    if "?"in str(message).lower():
-        return "Interesting question, I'll talk to you about that later - Bot Constantine"
+    if str(message).lower() == 'leb' or 'kaleb':
+        return "maaf kaleb sedang afk.\n_pesan ini dikirim otomatis mohon menunggu !!_"
 
-    else:
-        if "hi"in str(message).lower():
-            return "Hey, a bit busy right now. just drop a message ill talk to you later- Bot Constantine"
-        elif "happy new year"in str(message).lower():
-            return "Wishing you and your family a very Happy New Year! ✨.May this year bring joy and happiness in your life! - Bot Constantine "
-        elif "good morning"in str(message).lower():
-            return"Good Morning! Have a great day"
-        elif "good night"in str(message).lower():
-            return"Good Night! Sleep tight"
-        else :
-            return "Hello! Dear Sir/Madam, Nishanth is currently unavailable to reply. Do drop your messages. - Bot Constantine"
+    # else:
+    #     if "hi"in str(message).lower():
+    #         return "Hey, a bit busy right now. just drop a message ill talk to you later- Bot Constantine"
+    #     elif "happy new year"in str(message).lower():
+    #         return "Wishing you and your family a very Happy New Year! ✨.May this year bring joy and happiness in your life! - Bot Constantine "
+    #     elif "good morning"in str(message).lower():
+    #         return"Good Morning! Have a great day"
+    #     elif "good night"in str(message).lower():
+    #         return"Good Night! Sleep tight"
+    #     else :
+    #         return "Hello! Dear Sir/Madam, Nishanth is currently unavailable to reply. Do drop your messages. - Bot Constantine"
 
 
-#new messages check
+# new messages check
 def check_for_new_messages():
     pt.moveTo(x + 50, y - 45, duration=.5)
 
     while True:
         try:
-            position = pt.locateOnScreen("circle.png", confidence = .7)
+            position = pt.locateOnScreen("circle.png", confidence=.7)
 
             if position is not None:
                 pt.moveTo(position)
-                pt.moveRel(-100,0)
+                pt.moveRel(-100, 0)
                 pt.click()
                 processed_message = process_response(get_message())
                 post_response(processed_message)
+                after_end_message()
                 sleep(10)
 
             else:
                 print("No messages")
                 sleep(10)
 
-        except(Exception):
+        except (Exception):
             print("No new messages")
 
 
-
-
-
 check_for_new_messages()
-#processed_message = process_response(get_message())
-#post_response(processed_message)
+# processed_message = process_response(get_message())
+# post_response(processed_message)
